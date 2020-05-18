13f8430466f092ff2b53abb3ec3e9f5b195026eb
RindiRio95 <a19anjst@student.his.se>
2020-05-18 13:17:32 +0200

    La till en knapp

--- app/src/main/java/com/example/my_app/SecondActivity.java
+++ app/src/main/java/com/example/my_app/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.my_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button close = findViewById(R.id.go_back);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
